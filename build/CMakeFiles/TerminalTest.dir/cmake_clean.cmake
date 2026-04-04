2a506217ba9cb9343830b6115d8e5167b2e9387a
Carlos Sandoval <carlitoxsandoval8706@gmail.com>
2026-04-04 04:42:59 -0600

    Changed the name of the tester executable

---
+++ build/CMakeFiles/TerminalTest.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TerminalTest.dir/src/terminal/CommandPrompt.cpp.obj"
+  "CMakeFiles/TerminalTest.dir/src/terminal/CommandPrompt.cpp.obj.d"
+  "CMakeFiles/TerminalTest.dir/src/terminal/Dropdwn.cpp.obj"
+  "CMakeFiles/TerminalTest.dir/src/terminal/Dropdwn.cpp.obj.d"
+  "CMakeFiles/TerminalTest.dir/src/terminal/Typing.cpp.obj"
+  "CMakeFiles/TerminalTest.dir/src/terminal/Typing.cpp.obj.d"
+  "CMakeFiles/TerminalTest.dir/src/testing.cpp.obj"
+  "CMakeFiles/TerminalTest.dir/src/testing.cpp.obj.d"
+  "TerminalTest.exe"
+  "TerminalTest.exe.manifest"
+  "TerminalTest.pdb"
+  "libTerminalTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TerminalTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TerminalTest.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for TerminalTest.
+# This may be replaced when dependencies are built.
