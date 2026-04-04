1117962ab9889c1a857cc60ffbc3345daa0fc161
Carlos Sandoval <carlitoxsandoval8706@gmail.com>
2026-04-03 22:41:19 -0600

    I think it works now (terminal)

---
+++ build/CMakeFiles/TerminalGame.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TerminalGame.dir/src/terminal.cpp.obj"
+  "CMakeFiles/TerminalGame.dir/src/terminal.cpp.obj.d"
+  "TerminalGame.exe"
+  "TerminalGame.exe.manifest"
+  "TerminalGame.pdb"
+  "libTerminalGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TerminalGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
